4b19ded1004e55fd5b72e81daacb2d6eace463aa
afonsovictore-pro <afonsovictore@gmail.com>
2026-06-10 14:10:59 -0300

    Update questao06.py

--- questao06.py
+++ questao06.py
@@ -1,4 +1,4 @@
 numero = int(input("Digite um número entre 1 e 10: "))
 
-for i in range(1, 11):
-    print(f"{numero} X {i} = {numero * i}")
+for n in range(1, 11):
+    print(f"{numero} X {n} = {numero * n}")
