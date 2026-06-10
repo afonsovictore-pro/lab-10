2ddc30b141accca09e1ae96094fccb3eb195a973
afonsovictore-pro <afonsovictore@gmail.com>
2026-06-10 14:08:05 -0300

    Update questao02.py

--- questao02.py
+++ questao02.py
@@ -1,6 +1,6 @@
 # range(10) # inicio=0, fim=10, pulo=1
-for variavel in range(10):
-    print(variavel)
+for n in range(10):
+    print(n)
 # Resultado:
 # 0
 # 1
@@ -14,8 +14,8 @@ for variavel in range(10):
 # 9
 
 # range(1, 5) # inicio=1, fim=5, pulo=1
-for variavel in range(1, 5):
-    print(variavel)
+for n in range(1, 5):
+    print(n)
 # Resultado:
 # 1
 # 2
@@ -24,7 +24,7 @@ for variavel in range(1, 5):
 
 # range(0, 10, 2) # inicio=0, fim=10, pulo=2
 for variavel in range(0, 10, 2):
-    print(variavel)
+    print(n)
 # Resultado:
 # 0
 # 2
@@ -34,7 +34,7 @@ for variavel in range(0, 10, 2):
 
 # range(10, 0, -1) # inicio=10, fim=0, pulo=-1
 for variavel in range(10, 0, -1):
-    print(variavel)
+    print(n)
 # Resultado:
 # 10
 # 9
